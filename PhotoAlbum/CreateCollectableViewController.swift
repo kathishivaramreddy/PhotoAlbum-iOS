918936b4b461bcd5ee6b41f33ced50ec1bb2a5b7
ssh <kathishivaram@gmail.com>
2019-05-03 19:46:51 +0530

    save image of camera and media to image view

--- PhotoAlbum/CreateCollectableViewController.swift
+++ PhotoAlbum/CreateCollectableViewController.swift
@@ -8,26 +8,40 @@
 
 import UIKit
 
-class CreateCollectableViewController: UIViewController {
+class CreateCollectableViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
 
     @IBOutlet weak var imageView: UIImageView!
-    
     @IBOutlet weak var titleTextField: UITextField!
+    
+    private var photoPickerController = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        photoPickerController.delegate = self
     }
     
     
     @IBAction func addFromFolder(_ sender: UIBarButtonItem) {
+        photoPickerController.sourceType = .photoLibrary
+        present(photoPickerController, animated: true, completion: nil)
+        
+        
     }
     
     @IBAction func addFromCamera(_ sender: UIBarButtonItem) {
+        photoPickerController.sourceType = .camera
+        present(photoPickerController, animated: true, completion: nil)
     }
     
     @IBAction func AddImage(_ sender: UIButton) {
+        
     }
     
-    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            imageView.image = image
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
     
 }
