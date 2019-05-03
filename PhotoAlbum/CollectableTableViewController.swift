85591101dce1dce40d629e217c2088ebfa617956
ssh <kathishivaram@gmail.com>
2019-05-03 18:51:24 +0530

    setup contollers

---
+++ PhotoAlbum/CollectableTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CollectableTableViewController.swift
+//  PhotoAlbum
+//
+//  Created by apple on 5/3/19.
+//  Copyright © 2019 shivaapple. All rights reserved.
+//
+
+import UIKit
+
+class CollectableTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+}
---
+++ PhotoAlbum/CreateCollectableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CreateCollectableViewController.swift
+//  PhotoAlbum
+//
+//  Created by apple on 5/3/19.
+//  Copyright © 2019 shivaapple. All rights reserved.
+//
+
+import UIKit
+
+class CreateCollectableViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func addFromFolder(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func addFromCamera(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func AddImage(_ sender: UIButton) {
+    }
+    
+    
+    
+}
